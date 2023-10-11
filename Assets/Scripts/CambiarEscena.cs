828531ecbc223abb43f39959ee062fffe8749e14
Tadeo <tadeo@ravegan.com>
2023-10-10 22:03:30 -0300

    added a check in CambiarEscena para contra el player

--- Assets/Scripts/CambiarEscena.cs
+++ Assets/Scripts/CambiarEscena.cs
@@ -19,31 +19,12 @@ public class CambiarEscena : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!cambioDeEscenaRealizado)
+        if (!cambioDeEscenaRealizado && other.CompareTag("Player"))
         {
             PlayerData.Instance.playerPosition = nuevaPosisionJugador;
             PlayerData.Instance.playerRotation = Quaternion.Euler(nuevaRotacionJugador);
             SceneManager.LoadScene(escenaACargar);
             cambioDeEscenaRealizado = true;
-            // switch (other.tag)
-            // {
-            //     case "Trigger1To2":
-
-            //         PlayerData.Instance.playerPosition = new Vector3(-9f, 1f, 0f);
-            //         PlayerData.Instance.playerRotation = Quaternion.Euler(0f, 90f, 0f);
-            //         SceneManager.LoadScene(2);
-            //         cambioDeEscenaRealizado = true;
-            //         break;
-            //     case "Trigger2To1":
-
-            //         PlayerData.Instance.playerPosition = new Vector3(0f, 1f, 9f);
-            //         PlayerData.Instance.playerRotation = Quaternion.Euler(0f, 180f, 0f);
-            //         SceneManager.LoadScene(1);
-            //         cambioDeEscenaRealizado = true;
-            //         break;
-            //     default:
-            //         break;
-            // }
         }
     }
 }
